2cfa506f8a8a35507a94b01e1b058961f18b275e
ar1day <dtaradajko556@gmail.com>
2025-12-21 13:01:41 +0200

    add intro.py

---
+++ homework/homework1.py
@@ -0,0 +1,8 @@
+print("To be\nor not\nto be")
+
+print("'Life is what happens\n\twhen\n\tyou're busy making other plans' \nJohn Lennon")
+
+print("The best way to predict the future")
+print("\tis to")
+print("\t\tinvent it.")
+print("\tAlan Kay")

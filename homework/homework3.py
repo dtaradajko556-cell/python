fead8fd3c8cf763a1a39b673737480399e09ed97
ar1day <dtaradajko556@gmail.com>
2026-01-16 20:09:36 +0200

    add homework3.py

---
+++ homework/homework3.py
@@ -0,0 +1,45 @@
+# a = int(input("Введіть перше число: "))
+# b = int(input("Введіть друге число: "))
+# c = int(input("Введіть третє число: "))
+
+# suma = a + b + c
+# dobutok = a * b * c
+
+# print("Сума:", suma)
+# print("Добуток:", dobutok)
+
+# d1 = float(input("Введіть першу діагональ: "))
+# d2 = float(input("Введіть другу діагональ: "))
+
+# plosha = (d1 * d2) / 2
+
+# print("Площа ромба:", plosha)
+
+# zarplata = float(input("Введіть зарплату: "))
+# kredit = float(input("Введіть платіж за кредит: "))
+# komunalni = float(input("Введіть борг за комунальні послуги: "))
+
+# zalishok = zarplata - kredit - komunalni
+
+# print("Залишиться грошей:", zalishok)
+
+# idstan = float(input("Введіть відстань (км): "))
+# vitraty = float(input("Витрата палива на 100 км: "))
+# cina = float(input("Ціна за літр бензину: "))
+
+# palivo = (vidstan / 100) * vitraty
+# vartist = palivo * cina
+
+# print("Вартість поїздки:", vartist)
+
+# rahunok = float(input("Введіть суму рахунку: "))
+# lyudi = int(input("Введіть кількість осіб: "))
+
+# chayovi = rahunok * 0.15
+# zag_suma = rahunok + chayovi
+# na_odnogo = zag_suma / lyudi
+
+# print("Чайові:", chayovi)
+# print("Загальна сума:", zag_suma)
+# print("З кожного:", na_odnogo)
+

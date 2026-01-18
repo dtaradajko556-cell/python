cf3d35f57bdbdad1aac0a0a0c8dd809e333c5620
ar1day <dtaradajko556@gmail.com>
2026-01-18 14:16:06 +0200

    add qwerty.py

---
+++ class_work/conditions2.py
@@ -0,0 +1,65 @@
+a = 100
+b = 50
+
+# if a > b : print("a больше b")
+# 10 + 11 - бинарный оператор
+# num = -10
+# -num - унарный оператор
+
+# print("a > b") if a > b else print("a < b")
+
+# age = 17
+# can_vote = age >= 18 ? True : False
+# print("a > b") if a > b else (print("a < b") if a < b else print("a дорівнює b"))
+
+
+# login = input('Введіть ваш логін: ')
+
+# display_name = login if login else 'Гість'
+
+# print(f'Привіт, {display_name}!')
+
+# age = 17
+
+# if age < 18:
+#     pass
+# else:
+#     print("Full acsess granted")
+
+# day = int(input('Введіть номер дня тижня: '))
+
+# match day:
+#     case 1:
+#         print('Понеділок')
+#     case 2:
+#         print('Вівторок')
+#     case 3:
+#         print('Середа')
+#     case 4:
+#         print('Четвер')
+#     case 5:
+#         print("П'ятниця")
+#     case 6:
+#         print('Субота')
+#     case 7:
+#         print('Неділя')
+#     case _:
+#         print('Некоректний номер дня!')
+
+
+# month = int(input('Введіть номер місяця: '))
+
+# match month:
+#     case 12 | 1 | 2:
+#         print('Зима')
+#     case 3 | 4 | 5:
+#         print('Весна')
+#     case 6 | 7 | 8:
+#         print('Літо')
+#     case 9 | 10 | 11:
+#         print('Осінь')
+#     case _:
+#         print('Некоректний номер місяця!')
+
+
+
